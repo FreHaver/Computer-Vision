6a6848dc7a7f235ff7b1577a86382cde55d29074
LauraRuis <lauraruis@live.nl>
2018-02-18 15:33:58 +0100

    check integrab.

--- Lab 1/photometric/check_integrability.m
+++ Lab 1/photometric/check_integrability.m
@@ -4,32 +4,47 @@ function [ p, q, SE ] = check_integrability( normals )
 %   p : df / dx
 %   q : df / dy
 %   SE : Squared Errors of the 2 second derivatives
+
 % initalization
-p = zeros(size(normals));
-q = zeros(size(normals));
-SE = zeros(size(normals));
+% p = zeros(size(normals));
+[w, h, d] = size(normals);
+p = zeros(512, 512, 1);
+% q = zeros(size(normals));
+q = zeros(512, 512, 1);
+% SE = zeros(size(normals));
+SE = zeros(512, 512, 1);
 
 % ========================================================================
 % YOUR CODE GOES HERE
 % Compute p and q, where
 % p measures value of df / dx
 % q measures value of df / dy
-
-
+for row = 1:w
+    for col = 1:h
+        current = normals(row, col, :);
+        p(row, col, 1) = current(1) / current(3);
+        q(row, col, 1) = current(2) / current(3);
+    end
+end
 % ========================================================================
-
-
 
 p(isnan(p)) = 0;
 q(isnan(q)) = 0;
-
-
 
 % ========================================================================
 % YOUR CODE GOES HERE
 % approximate second derivate by neighbor difference
 % and compute the Squared Errors SE of the 2 second derivatives SE
-
+p2 = diff(p,1,1);
+q2 = diff(q,1,2);
+p2 = [p2; zeros(1, w)];
+q2 = [q2, zeros(w, 1)];
+for row = 1:w
+    for col = 1:h
+        errors = (p2(row, col) - q2(row, col))^2;
+        SE(row, col) = errors;
+    end
+end
 
 % ========================================================================
 
--- Lab 1/photometric/estimate_alb_nrm.m
+++ Lab 1/photometric/estimate_alb_nrm.m
@@ -11,27 +11,29 @@ function [ albedo, normal ] = estimate_alb_nrm( image_stack, scriptV, shadow_tri
 
 [h, w, ~] = size(image_stack);
 if nargin == 2
-    shadow_trick = true;
+    shadow_trick = true;    
 end
 
-% create arrays for 
-%   albedo (1 channel)
-%   normal (3 channels)
 albedo = zeros(h, w, 1);
 normal = zeros(h, w, 3);
 
-% =========================================================================
-% YOUR CODE GOES HERE
-% for each point in the image array
-%   stack image values into a vector i
-%   construct the diagonal matrix scriptI
-%   solve scriptI * scriptV * g = scriptI * i to obtain g for this point
-%   albedo at this point is |g|
-%   normal at this point is g / |g|
-
-
-
-% =========================================================================
+for row = 1:w
+    for col = 1:h
+        i = squeeze(image_stack(row, col, :));
+        scriptI = diag(i);
+        
+        if shadow_trick
+            [g, ~] = linsolve(scriptI * scriptV, scriptI * i);
+        else
+            [g, ~] = linsolve(scriptV, i);
+        end
+        
+        albedo(row, col, 1) = norm(g);
+        if norm(g) > 0
+            normal(row, col, :) = g / norm(g);
+        end
+    end
+end
 
 end
 
