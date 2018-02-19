1bea997ad7ab55da75fb009e6f6db7a4e5bfe785
LauraRuis <lauraruis@live.nl>
2018-02-19 17:09:47 +0100

    1dne

--- Lab 1/photometric/photometric_stereo.m
+++ Lab 1/photometric/photometric_stereo.m
@@ -6,17 +6,18 @@ disp('Part 1: Photometric Stereo')
 
 % obtain many images in a fixed view under different illumination
 disp('Loading images...')
-image_dir = './SphereGray5/';   % TODO: get the path of the script
+image_dir = './SphereColor/';   % TODO: get the path of the script
 %image_ext = '*.png';
 
-[image_stack, scriptV] = load_syn_images(image_dir);
+n_channels = 2;
+[image_stack, scriptV] = load_syn_images(image_dir, n_channels);
 [h, w, n] = size(image_stack);
 fprintf('Finish loading %d images.\n\n', n);
 
 % compute the surface gradient from the stack of imgs and light source mat
 disp('Computing surface albedo and normal map...')
-[albedo, normals] = estimate_alb_nrm(image_stack, scriptV);
 
+[albedo, normals] = estimate_alb_nrm(image_stack, scriptV, true);
 
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
 disp('Integrability checking')
@@ -25,9 +26,10 @@ disp('Integrability checking')
 threshold = 0.005;
 SE(SE <= threshold) = NaN; % for good visualization
 fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
+fprintf('Number of albedo errors: %d\n\n', sum(sum(abs(albedo) > 1)));
 
 %% compute the surface height
-height_map = construct_surface( p, q );
+height_map = construct_surface( p, q , 'average');
 
 %% Display
 show_results(albedo, normals, SE);
@@ -39,8 +41,8 @@ show_model(albedo, height_map);
 [h, w, n] = size(image_stack);
 fprintf('Finish loading %d images.\n\n', n);
 disp('Computing surface albedo and normal map...')
-[albedo, normals] = estimate_alb_nrm(image_stack, scriptV);
-
+[albedo, normals] = estimate_alb_nrm(image_stack, scriptV, false);
+ 
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
 disp('Integrability checking')
 [p, q, SE] = check_integrability(normals);
@@ -49,9 +51,10 @@ threshold = 0.005;
 SE(SE <= threshold) = NaN; % for good visualization
 fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
 
+ 
 %% compute the surface height
 height_map = construct_surface( p, q );
-
+ 
 show_results(albedo, normals, SE);
 show_model(albedo, height_map);
 
