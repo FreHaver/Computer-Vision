e787d8aaaefb4b267107e38d6c1503c32dc131da
Fredie <frediehaver@hotmail.com>
2018-02-21 10:21:13 +0100

    color constancy finished

--- Lab 1/color_constancy/AWB.m
+++ Lab 1/color_constancy/AWB.m
@@ -5,4 +5,31 @@ close all
 fprintf('Automatic White Balance\n')
 
 original_image = imread('awb.jpg');
-image(original_image)
+
+original_mean_vector = squeeze(mean(mean(original_image)));
+R_mean = original_mean_vector(1);
+G_mean = original_mean_vector(2);
+B_mean = original_mean_vector(3);
+fprintf('mean of the original image per channel: %f %f %f \n', R_mean, G_mean, B_mean)
+
+grey_world_image = zeros(size(original_image));
+
+grey_world_image(:, :, 1) = (128 / R_mean) * original_image(:, :, 1);
+grey_world_image(:, :, 2) = (128 / G_mean) * original_image(:, :, 2);
+grey_world_image(:, :, 3) = (128 / B_mean) * original_image(:, :, 3);
+
+grey_mean_vector = squeeze(mean(mean(grey_world_image)));
+R_mean2 = grey_mean_vector(1);
+G_mean2 = grey_mean_vector(2);
+B_mean2 = grey_mean_vector(3);
+
+grey_world_image = uint8(grey_world_image);
+fprintf('mean of the grey world image per channel: %f %f %f \n', R_mean2, G_mean2, B_mean2)
+figure;
+subplot(1,2,1);
+imshow(original_image);
+title('original');
+subplot(1,2,2);
+imshow(grey_world_image);
+title('grey world');
+
