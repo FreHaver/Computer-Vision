2d96fdcb18b732487bd62d845a2e366fc8d77757
Fredie <frediehaver@hotmail.com>
2018-02-28 09:18:12 +0100

    gabor visualization

--- Lab-2/Part-1_ Image Enhancement/compute_gradient.m
+++ Lab-2/Part-1_ Image Enhancement/compute_gradient.m
@@ -1,5 +1,11 @@
 function [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image)
-fprintf('Not implemented\n')
-
+	Gx_filter = [1 0 -1 ; 2 0 -2 ; 1 0 -1]; 
+    Gy_filter = [1 2 1 ; 0 0 0 ; -1 -2 -1];
+    Gx = imfilter(image, Gx_filter);
+    Gy = imfilter(image, Gy_filter);
+    Gx_squared = Gx.^2;
+    Gy_squared = Gy.^2;
+    im_magnitude = sqrt(Gx_squared + Gy_squared);
+    im_direction = atan(Gy ./ Gx);
 end
 
