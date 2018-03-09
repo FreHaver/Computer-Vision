97f7c08fdc5b5a7196934f6591d2579cd3383a3d
LauraRuis <lauraruis@live.nl>
2018-03-09 16:26:43 +0100

    alignment working

--- Lab-4/demo_function.m
+++ Lab-4/demo_function.m
@@ -1,3 +1,4 @@
+%% affine transformation
 clear all
 close all
 clc
@@ -8,41 +9,47 @@ Ib = imread('boat2.pgm');
 
 % use the keypoint_matching function to find the matching points in the two
 % images.
-[matches, scores, x_a, x_b, y_a, y_b] = keypoint_matching(Ia,Ib);
+[matches, scores, x_a, x_b, y_a, y_b, f_a, f_b] = keypoint_matching(Ia, Ib);
 
 % use the matching points from keypoint_matching to find the transformation
 % from image 1 to image 2.
-[m_remember, t_remember, n_inliers, a_inliers, b_inliers] = ransac(x_a, x_b, y_a, y_b, 50, 20);
+[m_remember, t_remember, n_inliers, a_inliers, b_inliers] = ransac(x_a, x_b, y_a, y_b, 50, 50);
 
 fprintf('number of inliers: %i \n', n_inliers); 
 
-% [h,w] = size(Ia);
-% new_Ia = zeros(h,w);
-% count = 0;
-% for row = 1:h
-%     for col = 1:w
-%         new_coordinates = m_remember * [row; col] + t_remember;
-%         new_row = round(new_coordinates(1));
-%         new_col = round(new_coordinates(2));
-%         if (0 < new_row) && (new_row < h) && (0 < new_col) && (new_col < w)
-% %             fprintf('Old row and column: %i, %i. New row and column: %i, %i \n', row, col, new_row, new_col)
-%             new_Ia(new_row, new_col) = Ia(row,col);
-%         else
-%             count = count +1;
-%         end
-%     end
-% end
+[h,w] = size(Ia);
+new_Ia = zeros(h, w);
+count = 0;
+for row = 1:h
+    for col = 1:w
+        new_coordinates = m_remember * [row; col] + t_remember;
+        new_row = round(new_coordinates(1));
+        new_col = round(new_coordinates(2));
+        if new_row <= 0
+            new_row = 1;
+        end
+        if new_col <= 0
+            new_col = 1;
+        end
+        new_Ia(new_row, new_col) = Ia(row, col);
+    end
+end
+
+% result comparison with matlab built-in functions (used imwarp instead of 
 m_trans = horzcat(vertcat(m_remember, t_remember'), [0; 0; 1]);
 T = affine2d(m_trans);
-new_Ia = imwarp(Ia, T);
-% fprintf('count = %i \n', count)
+new_Ia2 = imwarp(Ia, T);
+
 % create a new figure
 figure(3) ; clf ;
 % show the two images side to side
 imagesc(new_Ia);
+figure(4) ; clf ;
+imagesc(new_Ia2);
+
 % figure(4) ; clf ;
 % imagesc(Ib);
-% imagesc(cat(2, Ia, Ib)) ;
+% imagesc(cat(2, Ib, new_Ia)) ;
 
 % get the x and y coordinates of the inliers, (the x coordinates for the
 % second image are shifted because the images are shown side by side)
@@ -53,6 +60,6 @@ yb = b_inliers(2,:);
 
 % draw lines between the interest points in image 1 and the interest points
 % in image 2. 
-hold on ;
-h = line([xa ; xb], [ya ; yb]) ;
-set(h,'linewidth', 1, 'color', 'b') ;
+% hold on ;
+% h = line([xa ; xb], [ya ; yb]) ;
+% set(h,'linewidth', 1, 'color', 'b') ;
--- Lab-4/keypoint_matching.m
+++ Lab-4/keypoint_matching.m
@@ -1,54 +1,52 @@
-function [matches, scores, x_a, x_b, y_a, y_b] = keypoint_matching(image1, image2)
+function [matches, scores, x_a, x_b, y_a, y_b, fa,fb] = keypoint_matching(image1, image2)
 
-% convert the image to single precision
-Ia_single = single(image1);
-Ib_single = single(image2);
+    % convert the image to single precision
+    Ia_single = single(image1);
+    Ib_single = single(image2);
 
-% the function vl_sift is used to compute the SIFT frames (the keypoints),
-% and the descriptors
-[fa, da] = vl_sift(Ia_single);
-[fb, db] = vl_sift(Ib_single);
+    % the function vl_sift is used to compute the SIFT frames (the keypoints),
+    % and the descriptors
+    [fa, da] = vl_sift(Ia_single);
+    [fb, db] = vl_sift(Ib_single);
 
-% with the descriptors, the frames are matched and given a score
-[matches, scores] = vl_ubcmatch(da, db);
+    % with the descriptors, the frames are matched and given a score
+    [matches, scores] = vl_ubcmatch(da, db);
 
-% with the matches and the frames, the x and y coordinates of the frames in
-% both images are stored.
-[h,w] = size(matches);
+    % with the matches and the frames, the x and y coordinates of the frames in
+    % both images are stored.   
+    x_a = fa(2, matches(1, :)) ;
+    x_b = fb(2, matches(2, :)) ;
+    y_a = fa(1, matches(1, :)) ;
+    y_b = fb(1, matches(2, :)) ;
 
-x_a = fa(1,matches(1,:)) ;
-x_b = fb(1,matches(2,:)) ;
-y_a = fa(2,matches(1,:)) ;
-y_b = fb(2,matches(2,:)) ;
+    % the scores are sorted in descending order and also the matches are sorted
+    % accordingly
+    [~, perm] = sort(scores, 'descend') ;
+    matches = matches(:, perm);
+    scores  = scores(perm);
 
-% the scores are sorted in descending order and also the matches are sorted
-% accordingly
-[drop, perm] = sort(scores, 'descend') ;
-matches = matches(:, perm);
-scores  = scores(perm);
+    % create a figure with the two images side by side
+    figure(1) ; clf ;
+    imagesc(cat(2, image1, image2)) ;
+    axis image off ;
 
-% create a figure with the two images side by side
-figure(1) ; clf ;
-imagesc(cat(2, image1, image2)) ;
-axis image off ;
+    % show the images again side by side, but this time with lines and points
+    % to mark the matches (only the 50 with the highest scores are shown)
+    matches = matches(:, 1:50);
+    figure(2) ; clf ;
+    imagesc(cat(2, image1, image2)) ;
 
-% show the images again side by side, but this time with lines and points
-% to mark the matches (only the 50 with the highest scores are shown)
-matches = matches(:, 1:50);
-figure(2) ; clf ;
-imagesc(cat(2, image1, image2)) ;
+    xa = fa(1, matches(1, :)) ;
+    xb = fb(1, matches(2, :)) + size(image1, 2) ;
+    ya = fa(2, matches(1, :)) ;
+    yb = fb(2, matches(2, :)) ;
 
-xa = fa(1,matches(1,:)) ;
-xb = fb(1,matches(2,:)) + size(image1,2) ;
-ya = fa(2,matches(1,:)) ;
-yb = fb(2,matches(2,:)) ;
+    hold on ;
+    h = line([xa ; xb], [ya ; yb]) ;
+    set(h,'linewidth', 1, 'color', 'b') ;
 
-hold on ;
-h = line([xa ; xb], [ya ; yb]) ;
-set(h,'linewidth', 1, 'color', 'b') ;
-
-vl_plotframe(fa(:,matches(1,:))) ;
-fb(1,:) = fb(1,:) + size(image1,2) ;
-vl_plotframe(fb(:,matches(2,:))) ;
-axis image off ;
+    vl_plotframe(fa(:,matches(1,:))) ;
+    fb(1,:) = fb(1,:) + size(image1,2) ;
+    vl_plotframe(fb(:,matches(2,:))) ;
+    axis image off ;
 end
