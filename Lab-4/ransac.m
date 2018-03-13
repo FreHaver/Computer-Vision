0be453002224ef9934bd281a8547b90b44b7aaee
LauraRuis <lauraruis@live.nl>
2018-03-13 09:26:47 +0100

    added last iteration of least squares with inliers

--- Lab-4/ransac.m
+++ Lab-4/ransac.m
@@ -1,4 +1,7 @@
 function [m_remember, t_remember, max_inliers, a_remember, b_remember] = ransac(x_a, x_b, y_a, y_b, p, n)
+
+    % TODO: calc least squares for final pars and inliers
+    
     max_inliers = 0;
     for i=1:n
         fprintf('n is %i \n', i)
@@ -20,26 +23,11 @@ function [m_remember, t_remember, max_inliers, a_remember, b_remember] = ransac(
         b_inliers = zeros(2,1);
         sample_size = length(sample_x_a);
         
-        A = zeros(2*p, 6);
-        b = zeros(2*p, 1);
-        for j=1:p
-            %initialization 
-            inliers = 0;
-            a_inliers = zeros(2,1);
-            b_inliers = zeros(2,1);
-            
-            % construct the A matrix and b vector and from those compute
-            % the x vector (which is used to construct the m matrix and t
-            % vector)
-            A(j+j-1:j+j, :) = [sample_x_a(j), sample_y_a(j), 0, 0, 1, 0; 0, 0, sample_x_a(j), sample_y_a(j), 0, 1];
-            b(j+j-1:j+j, :) = [sample_x_b(j); sample_y_b(j)];
-        end
+        [A, b] = construct_matrices(sample_x_a, sample_y_a, sample_x_b, sample_y_b, p);
         
         x = pinv(A)*b;
-        m_matrix = [x(1), x(2); x(3), x(4)];
-        t_vector = [x(5); x(6)];
-
-        
+        [m_matrix, t_vector] = get_transform_pars(x);
+                
         %loop over all the interest points found by keypoint_matching
         for m = 1:le
             % transform the points from image 1 (a) with the m_matrix
@@ -67,10 +55,36 @@ function [m_remember, t_remember, max_inliers, a_remember, b_remember] = ransac(
         if inliers > max_inliers
             max_inliers = inliers;
             fprintf('max_inliers is %i \n', max_inliers)
-            m_remember = m_matrix;
-            t_remember = t_vector;
             a_remember = a_inliers(:,2:length(a_inliers));
             b_remember = b_inliers(:,2:length(b_inliers));
         end
     end
+    
+    % perform one last iteration of least squares on only inliers
+    xa = a_inliers(1,:);
+    xb = b_inliers(1,:);
+    ya = a_inliers(2,:);
+    yb = b_inliers(2,:);
+    p = size(xa);
+    [A, b] = construct_matrices(xa, ya, xb, yb, p(2));
+    x = pinv(A)*b;
+    [m_remember, t_remember] = get_transform_pars(x);
+    
+end
+
+function [A, b] = construct_matrices(x_a, y_a, x_b, y_b, p)
+    A = zeros(2*p, 6);
+    b = zeros(2*p, 1);
+    for j=1:p            
+        % construct the A matrix and b vector and from those compute
+        % the x vector (which is used to construct the m matrix and t
+        % vector)
+        A(j+j-1:j+j, :) = [x_a(j), y_a(j), 0, 0, 1, 0; 0, 0, x_a(j), y_a(j), 0, 1];
+        b(j+j-1:j+j, :) = [x_b(j); y_b(j)];
+    end
+end
+
+function [m, t] = get_transform_pars(x)
+    m = [x(1), x(2); x(3), x(4)];
+    t = [x(5); x(6)];
 end
